7003116b60bcbbf638a204f9823e0b83c70eaaaa
ilene97 <ilene97@naver.com>
2019-10-27 18:06:01 +0900

    Naver mail pasing

--- MailDownloads_POP3/download_emails.py
+++ MailDownloads_POP3/download_emails.py
@@ -1,14 +1,13 @@
 import poplib
 from email.parser import Parser
-from email.header import decode_header
-from email.utils import parseaddr
 import base64
 from bs4 import BeautifulSoup
 import quopri
 
 poplib._MAXLINE=204800
 
-def UTFdecoding(msg):
+
+def utf_decoding(msg):
     msg = msg.replace("=?UTF-8?B?","")
     msg = msg.replace("=?utf-8?B?", "")
     msg = msg.replace("?=", "")
@@ -17,7 +16,8 @@ def UTFdecoding(msg):
     msg = msg.decode('utf-8')
     return msg
 
-def EUC_KRdecoding(msg):
+
+def eur_kr_decoding(msg):
     msg = msg.replace("=?euc-kr?B?", "")
     msg = msg.replace("=?EUC-KR?B?", "")
     msg = msg.replace("?=", "")
@@ -26,17 +26,46 @@ def EUC_KRdecoding(msg):
     msg = msg.decode('euc-kr')
     return msg
 
-def price_from_html_for_naverpay(msg_content):
-    html_doc = msg_content
-    start_index = html_doc.find("<html")
-    end_index = html_doc.rfind('/html>') + 5
-    real_content = html_doc[start_index:end_index + 1]
-    soup = BeautifulSoup(real_content,'html.parser')
+
+def info_from_html_for_naverpay(msg_content):
+    global price, purchasing_item, purchasing_office
+    soup = BeautifulSoup(msg_content,'html.parser')
     soup.prettify()
-    print(real_content)
-    print()
-    for tag in soup.find_all('span'):
-        print(tag.text)
+    # 결제금액
+    for tag in soup.find_all('td'):
+        temp = quopri.decodestring(tag.text)
+        temp = temp.decode('utf-8')
+        if "최종결제금액" in temp:
+            price_index = temp.find("최종결제금액")
+            price_content = temp[price_index + 6:]
+            price = price_content.strip()
+            break
+    # case1:주문상품명만
+    if soup.find('html') is None:
+        for tag in soup.find_all('td'):
+            temp = quopri.decodestring(tag.text)
+            temp = temp.decode('utf-8')
+            if "주문상품" in temp:
+                purchasing_index = temp.find("주문상품")
+                purchasing_item = temp[purchasing_index + 4:]
+                purchasing_item = purchasing_item.strip()
+                purchasing_item = purchasing_item[:50].strip()
+                purchasing_office = "none"
+                break
+    # case2:주문상품명,구매상점명
+    else:
+        for tag in soup.find_all('td'):
+            temp = quopri.decodestring(tag.text)
+            temp = temp.decode('utf-8')
+            if "판매자" in temp:
+                purchasing_office_index = temp.find("판매자")
+                purchasing_office = temp[purchasing_office_index + 10:purchasing_office_index + 150]
+                purchasing_office = purchasing_office.strip()
+                purchasing_item_index = temp.find("상품정보")
+                purchasing_item = temp[purchasing_item_index + 10:]
+                purchasing_item = purchasing_item.strip()
+                break
+
 
 def info_from_html_for_KG(msg_content):
     global price, purchasing_item, purchasing_office
@@ -50,6 +79,7 @@ def info_from_html_for_KG(msg_content):
     price = soup.select('td[colspan="2"]')[1].text.strip()
     purchasing_item = soup.select('td[width="242"]')[1].text
     purchasing_office = soup.select('td[width="242"]')[2].text
+
 
 def info_from_html_for_NHN(msg_content):
     global price, purchasing_item, purchasing_office
@@ -75,19 +105,18 @@ def info_from_html_for_NHN(msg_content):
     purchasing_item = soup.find('td').text
 
 
-
 # input email address, password and pop3 server domain or ip address
 
-email ="ID"
+email = "ID"
 password = "PW"
 
 pop3_server = "pop.naver.com"
 
 # pop3서버 접근, pop3포트 번호 995:
-server = poplib.POP3_SSL('pop.naver.com',995)
+server = poplib.POP3_SSL('pop.naver.com', 995)
 
 # open debug switch to print debug information between client and pop3 server.
-#server.set_debuglevel(1)
+# server.set_debuglevel(1)
 
 '''
 # get pop3 server welcome message.
@@ -109,7 +138,7 @@ index = len(mails)
 print("length of index : ",index)
 print()
 
-#declare variables for db
+# declare variables for db
 company = "PG"
 date = "yyyy.MM.DD"
 price = "0"
@@ -121,7 +150,7 @@ for i in range(index):
     resp, lines, octets = server.retr(index-i)
     try:
         msg_content = b'\r\n'.join(lines).decode('utf-8')
-    except UnicodeDecodeError :
+    except UnicodeDecodeError:
         try:
             msg_content = b'\r\n'.join(lines).decode('euc-kr')
         except:
@@ -130,10 +159,10 @@ for i in range(index):
     msg = Parser().parsestr(msg_content)
     email_from = msg.get('From')
 
-    #KG이니시스
+    # KG이니시스
     if "S0fAzLTPvcO9ug" in email_from:
         email_title = msg.get('Subject')
-        email_title = EUC_KRdecoding(email_title)
+        email_title = eur_kr_decoding(email_title)
         if "결제" in email_title:
             print("<", i, ">")
             email_date = msg.get('Date')
@@ -146,16 +175,39 @@ for i in range(index):
             print('Subject ' + email_title)
             info_from_html_for_KG(msg_content)
             print('구매상점명 '+purchasing_office)
-            print('주문상품명'+purchasing_item)
+            print('주문상품명 '+purchasing_item)
             print('Price ' + price)
             print()
         else:
             pass
 
-    #NHN KCP
+    # Naverpay
+    if "64Sk7J2067KE7Y6Y7J20" in email_from:
+        email_title = msg.get('Subject')
+        email_title = utf_decoding(email_title)
+        if "결제" in email_title:
+            print("<", i, ">")
+            email_date = msg.get('Date')
+            try:
+                print('Date ' + email_date)
+            except TypeError:
+                print("type error")
+            print('From 네이버페이')
+            print('Subject ' + email_title)
+            info_from_html_for_naverpay(msg_content)
+            print('구매상점명 ' + purchasing_office)
+            print('주문상품명 ' + purchasing_item)
+            print('가격 ' + price)
+            #print(msg_content)
+            print()
+            # print('To ' + email_to)
+        else:
+            pass
+
+    # NHN KCP
     elif "NHN KCP" in email_from:
         email_title = msg.get('Subject')
-        email_title = EUC_KRdecoding(email_title)
+        email_title = eur_kr_decoding(email_title)
         if "결제" in email_title:
             print("<", i, ">")
             email_date = msg.get('Date')
@@ -167,7 +219,7 @@ for i in range(index):
             print('Subject ' + email_title)
             info_from_html_for_NHN(msg_content)
             print('구매상점명 ' + purchasing_office)
-            print('주문상품명' + purchasing_item)
+            print('주문상품명 ' + purchasing_item)
             print('Price ' + price)
             #print(msg_content)
             print()
@@ -176,7 +228,6 @@ for i in range(index):
 
     else:
         pass
-
 
 
 # delete the email from pop3 server directly by email index.
