ec56e22d2ec9746ad990e39bb45e31b9861b3592
ilene97 <ilene97@naver.com>
2019-11-09 19:45:41 +0900

    include texts msg

--- GoogleVisionOCRtest/bankocr.py
+++ GoogleVisionOCRtest/bankocr.py
@@ -1,56 +1,10 @@
 import io   # 파일을 읽고 쓰기위한 모듈
 import re
-
 # Imports the Google Cloud client library
 from google.cloud import vision
 
-client = vision.ImageAnnotatorClient()
 
-with io.open('file path', 'rb') as image_file:
-    content = image_file.read()
-
-image = vision.types.Image(content=content)
-
-response = client.text_detection(image=image)
-texts = response.text_annotations
-
-wholeText = texts[0].description
-print("< ocr 결과 >")
-print(wholeText)
-print()
-tokens = wholeText.split('\n')
-
-date_regular = re.compile('\d+[/]\d+[/]*\d*|\d+[.]\d+[.]*\d*|\d+월\s*\d+일')
-price_regular = re.compile('-\d*[,]*\d+\s*원|출금\s*\d*[,]*\d+\s*원')
-price_regular2 = re.compile('(\d*[,]*)*\d+\s*원|(\d*[,]*)*\d+|[\\\](\d*[,]*)*\d+|W(\d*[,]*)*\d+')
-price_regular3 = re.compile('(\d*[,]*)*\d+\s*원')
-
-numbers = []
-valid = False           # 간편결제 or PG사 지출이 맞는지
-changePrice = False     # for 똑똑가계부 \처리
-
-
-for token in tokens:
-    if "이니시스" in token:
-        company = "이니시스"
-        valid = True
-        break
-    elif "네이버페이" in token:
-        company = "네이버페이"
-        valid = True
-        break
-    elif "NHN" in token or "KCP" in token:
-        company = "NHN KCP"
-        valid = True
-        break
-
-# 입금이라는 단어가 있을경우 지출내역 아니므로 지출금액 찾지 않음
-for token in tokens:
-    if "입금" in token:
-        valid = False
-
-# 날짜 찾기
-if valid:
+def find_date(tokens):
     # print("<날짜>")
     for token in tokens:
         m = date_regular.search(token)
@@ -61,6 +15,7 @@ if valid:
                 year = temp[0]
                 month = temp[1]
                 day = temp[2]
+                break
 
             elif len(temp) == 2:
                 if len(temp[0]) > 3:
@@ -71,12 +26,14 @@ if valid:
                     year = '0'
                     month = temp[0]
                     day = temp[1]
+                break
 
-            date = year + "." + month + "." + day
-            print(date)
+    date = year + "." + month + "." + day
+    print(date)
 
-# 지출금액 찾기
-if valid:
+
+def find_price_app(tokens):
+    global numbers
     # print("<금액>")
     # - ~ 원 혹은 출금 ~ 원 인 경우 우선 추출 (한개만)
     for token in tokens:
@@ -119,3 +76,94 @@ if valid:
     findPrice = re.compile('[0-9]')
     price = "".join(findPrice.findall(price))
     print(price)
+
+
+def find_price_text(tokens):
+    global numbers
+    # print("<금액>")
+    price_candidates = []
+    for token in tokens:
+        temp = token.split()
+        for i in temp:
+            numbers.append(i)
+    # - ~ 원 혹은 출금 ~ 원 인 경우 우선 추출 (한개만)
+    for i in numbers:
+        m = price_regular.search(i)
+        if m is not None:
+            price_candidates.append(m.group())
+            break
+    # 한개 있는 경우
+    if len(price_candidates) == 1:
+        price = price_candidates[0]
+        # print(price)
+    # - ~ 원 혹은 출금 ~ 원 문자열이 없는 경우
+    else:
+        for idx, val in enumerate(numbers):
+            if '원' in val:
+                price_candidates.append(val)
+        if len(price_candidates) == 1:
+            price = price_candidates[0]
+        else:
+            for i in price_candidates:
+                m = price_regular2.match(i)
+                if m is not None:
+                    price = i
+
+    findPrice = re.compile('[0-9]')
+    price = "".join(findPrice.findall(price))
+    print(price)
+
+client = vision.ImageAnnotatorClient()
+with io.open('file path', 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.types.Image(content=content)
+
+response = client.text_detection(image=image)
+texts = response.text_annotations
+
+wholeText = texts[0].description
+print("< ocr 결과 >")
+print(wholeText)
+print()
+tokens = wholeText.split('\n')
+
+date_regular = re.compile('\d+[/]\d+[/]*\d*|\d+[.]\d+[.]*\d*|\d+월\s*\d+일')
+price_regular = re.compile('-\d*[,]*\d+\s*원|출금\s*\d*[,]*\d+\s*원')
+price_regular2 = re.compile('(\d*[,]*)*\d+\s*원|(\d*[,]*)*\d+|[\\\](\d*[,]*)*\d+|W(\d*[,]*)*\d+')
+price_regular3 = re.compile('(\d*[,]*)*\d+\s*원')
+
+numbers = []
+valid = False           # 간편결제 or PG사 지출이 맞는지
+changePrice = False     # for 똑똑가계부 \처리
+texts_msg = False       # 문자인가
+
+
+for token in tokens:
+    if "이니시스" in token:
+        company = "이니시스"
+        valid = True
+        break
+    elif "네이버페이" in token:
+        company = "네이버페이"
+        valid = True
+        break
+    elif "NHN" in token or "KCP" in token:
+        company = "NHN KCP"
+        valid = True
+        break
+
+# 입금이라는 단어가 있을경우 지출내역 아니므로 지출금액 찾지 않음
+for token in tokens:
+    if "입금" in token:
+        valid = False
+    if "Web발신" in token:
+        texts_msg = True
+
+if valid:
+    find_date(tokens)    # 날짜 찾기
+    if texts_msg:
+        find_price_text(tokens)
+    else:
+        find_price_app(tokens)     # 결제금액 찾기
+
